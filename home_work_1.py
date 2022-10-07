4ed60bc4e0837af48f710edce6d79c567afe2ff2
Shirinf87 <106368719+Shirinf87@users.noreply.github.com>
2022-10-07 20:29:14 +0600

    Add files via upload

---
+++ home_work_1.py
@@ -0,0 +1,35 @@
+#Operator
+print(10 + 100)
+#output: 110
+
+#Python Arithmetic Operators (+,-,*,/,% Modulus,**Exponentiation, // floor Division)
+x = 5
+y = 2
+print(x % y)
+#output: 1
+
+x = 2
+y = 5
+print(x ** y) 
+#same as 2*2*2*2*2
+#output: 32
+
+x = 15
+y = 2
+print(x // y)
+#the floor division // rounds the result down to the nearest whole number
+#output: 7
+
+#Python Assignment Operators
+x = 5
+x >>= 3
+print(x)
+#output: 0
+
+x = 5
+x <<= 3
+print(x)
+#output: 40
+
+
+# Md. Shariful-Islam
